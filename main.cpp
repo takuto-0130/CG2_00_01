f4c254e09709784a2a81edaa162947de4f1cb430
匠人 山口 <k023g0005@g.neec.ac.jp>
2024-04-12 15:12:06 +0900

    ウィンドウを表示

--- main.cpp
+++ main.cpp
@@ -1,6 +1,72 @@
 #include <Windows.h>
+#include <cstdint>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+
+	switch (msg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+
+	//
+	WNDCLASS wc{};
+	wc.lpfnWndProc = WindowProc;
+
+	wc.lpszClassName = L"CG2WindowClass";
+
+	wc.hInstance = GetModuleHandle(nullptr);
+
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+	RegisterClass(&wc);
+	//
+
+	//
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHieght = 720;
+
+	RECT wrc = { 0, 0, kClientWidth, kClientHieght };
+
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+	//
+
+
+	//
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,
+		L"CG2",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrc.right - wrc.left,
+		wrc.bottom - wrc.top,
+		nullptr,
+		nullptr,
+		wc.hInstance,
+		nullptr);
+
+	ShowWindow(hwnd, SW_SHOW);
+	//
+
+
+	MSG msg{};
+	//
+	while (msg.message != WM_QUIT) {
+
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+
+		}
+	}
 
 	OutputDebugStringA("Hello,DirectX!\n");
 	return 0;
