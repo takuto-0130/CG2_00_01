27fd74909b3259154e5ff6fb0326918f3a4b7326
takuto-0130 <k023g0005@g.neec.ac.jp>
2024-06-05 11:50:17 +0900

    05_04 終わり

--- Object3d.PS.hlsl
+++ Object3d.PS.hlsl
@@ -29,7 +29,8 @@ PixelShaderOutput main(VertexShaderOutput input)
     PixelShaderOutput output;
     if (gMaterial.enableLighting != 0)
     {
-        float cos = saturate(dot(normalize(input.normal), -gDirectionalLight.direction));
+        float NdotL = dot(normalize(input.normal), -gDirectionalLight.direction);
+        float cos = pow(NdotL * 0.5f + 0.5f, 2.0f);
         output.color = gMaterial.color * textureColor * gDirectionalLight.color * cos * gDirectionalLight.intensity;
     }
     else { output.color = gMaterial.color * textureColor;}
