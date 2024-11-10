7a8fdecb32487093340fb59115671bf04d296b79
takuto <k023g0005@g.neec.ac.jp>
2024-11-10 22:32:08 +0900

    temp

---
+++ x64/Release/Object3d.VS.hlsl
@@ -0,0 +1,24 @@
+#include "Object3d.hlsli"
+
+struct TransfomationMatrix
+{
+    float4x4 WVP;
+    float4x4 World;
+};
+ConstantBuffer<TransfomationMatrix> gTransfomationMatrix : register(b0);
+
+struct VertexShaderInput
+{
+    float4 position : POSITION0;
+    float2 texCoord : TEXCOORD0;
+    float3 normal : NORMAL0;
+};
+
+VertexShaderOutput main(VertexShaderInput input)
+{
+    VertexShaderOutput output;
+    output.position = mul(input.position, gTransfomationMatrix.WVP);
+    output.texCoord = input.texCoord;
+    output.normal = normalize(mul(input.normal, (float3x3) gTransfomationMatrix.World));
+    return output;
+}
